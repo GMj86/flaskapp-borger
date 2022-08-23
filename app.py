c5248d8ced013b5e0be36f7728136fa4181a905b
gmanzano86 <gmanzano86@outlook.com>
2022-08-23 13:29:32 -0600

    moearar

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
+@app.route('/gateway')
+def gateway():
+    return render_template('gateway.html')
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8080)
