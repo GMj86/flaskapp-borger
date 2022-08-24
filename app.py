12030ea9d9f7837e16cf1af6d44e36f950b08e2b
gmanzano86 <gmanzano86@outlook.com>
2022-08-24 01:25:19 -0600

    first try on forms

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+
 app = Flask(__name__)
+
+struct = []
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-@app.route('/gateway')
+    return render_template('index.html', struct=struct)
+
+@app.route('/gateway', methods=['GET','POST'])
 def gateway():
+    if request.method == 'POST':
+        title = request.form['title']
+
+        if not title:
+            flash("Title required")
+
+        else:
+            struct.append(title)
+
     return render_template('gateway.html')
+
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8080)
