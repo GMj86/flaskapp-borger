3d325312e1c06998e86d4fc1fe7736d904ff2942
gmanzano86 <gmanzano86@outlook.com>
2022-08-27 10:48:22 -0600

    more debugs

--- app.py
+++ app.py
@@ -13,41 +13,44 @@ def index():
     global btn_count
 
     if request.method == 'post':
+        if request.form['btTxt submit'] == 'btn':
+            btn_count+=1
 
         if 'drink1' in request.form:
             btn_count += 1
             print('drink1 pressed')
-        if 'drink2' in request.form:
+        elif 'drink2' in request.form:
             print('drink2 pressed')
-        if 'drink3' in request.form:
+        elif 'drink3' in request.form:
             print('drink3 pressed')
-        if 'drink4' in request.form:
+        elif 'drink4' in request.form:
             print('drink4 pressed')
 
-        if 'side1' in request.form:
+        elif 'side1' in request.form:
             btn_count += 1
             print('side1 pressed')
-        if 'side2' in request.form:
+        elif 'side2' in request.form:
             print('side2 pressed')
-        if 'side3' in request.form:
+        elif 'side3' in request.form:
             print('side3 pressed')
-        if 'side4' in request.form:
+        elif 'side4' in request.form:
             print('side4 pressed')
 
-        if 'burger1' in request.form:
+        elif 'burger1' in request.form:
             btn_count += 1
             print('burger1 pressed')
-        if 'burger2' in request.form:
+        elif 'burger2' in request.form:
             print('burger2 pressed')
-        if 'burger3' in request.form:
+        elif 'burger3' in request.form:
             print('burger3 pressed')
-        if 'burger4' in request.form:
+        elif 'burger4' in request.form:
             print('burger4 pressed')
 
-        # else:
-        #     print('something not right')
+        else:
+            print('something not right')
+            
     print(btn_count)
-    return render_template('some.html', btn_count=btn_count)
+    return render_template('some.html', value1=btn_count)
 
 # @app.route('/gateway', methods=['GET','POST'])
 # def gateway():
