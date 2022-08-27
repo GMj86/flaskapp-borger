f687a0a7d86d61687270eae4341662687c548374
gmanzano86 <gmanzano86@outlook.com>
2022-08-27 10:07:38 -0600

    switching from post to get

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
     # return render_template('index.html', struct=struct, btn_count=btn_count,clear_count=clear_count)
     global btn_count
 
-    if request.method == 'POST':
+    if request.method == 'get':
 
         if 'drink1' in request.form:
             btn_count += 1
